cc273b4adacd09932437057c597d84fad025c89f
zhqhzhqh <zhqhzhqh@163.com>
2017-04-12 18:27:48 +0800

    add reader lib make version unit

---
+++ readerlibrary/src/main/java/com/kiss/readerlibrary/ReaderLibraryApplication.java
@@ -0,0 +1,11 @@
+package com.kiss.readerlibrary;
+
+import android.app.Application;
+
+/**
+ * @author qinghui
+ * @date 2017/3/30
+ */
+
+public class ReaderLibraryApplication extends Application {
+}
